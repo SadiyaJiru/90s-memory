6ce5223a65c71f818b45f7e8b9f1a837cc5e7519
sadiyaj <sadiyajiru@gmail.com>
2018-11-20 19:18:43 -0600

    Updates

---
+++ precache-manifest.d5d0067563190529347ca8948fbfd246.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "66770a3beec1c7cb201a",
+    "url": "/90s-memory/static/css/main.e2341acd.chunk.css"
+  },
+  {
+    "revision": "66770a3beec1c7cb201a",
+    "url": "/90s-memory/static/js/main.66770a3b.chunk.js"
+  },
+  {
+    "revision": "d855c10c80aa95827f2a",
+    "url": "/90s-memory/static/js/1.d855c10c.chunk.js"
+  },
+  {
+    "revision": "2f4fc01f5e6a5fc1222b",
+    "url": "/90s-memory/static/js/runtime~main.2f4fc01f.js"
+  },
+  {
+    "revision": "fb3504552bbba3116962163d9e14f2a4",
+    "url": "/90s-memory/static/media/pinkie-background.fb350455.jpg"
+  },
+  {
+    "revision": "e1d7e2d08e2a9495601bfaf07cccff22",
+    "url": "/90s-memory/index.html"
+  }
+];
